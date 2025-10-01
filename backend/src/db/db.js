f6d3027310ce9ce7c0605690d34506a4020fd62b
alokkkxpixel <alokpithale4@gmail.com>
2025-10-01 15:41:12 +0530

    mongo atlas hosted

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose");
 
-function connectDb() {
-  mongoose
-    .connect(process.env.MONGO_URI)
-
-    .then(() => {
-      console.log("Db connceted");
-    })
-    .catch((err) => {
-      console.log("db error", err);
-    });
-}
+ async function connectDb() {
+   try {
+     await mongoose.connect(process.env.MONGO_ATLAS_URI, {
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+     });
+     console.log("✅ MongoDB Atlas Connected...");
+   } catch (err) {
+     console.error("❌ Connection error:", err.message);
+     process.exit(1);
+   }
+ }
 
 module.exports = connectDb;
