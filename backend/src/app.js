59b2030ec724664a2c5383b8c9b1137c7922cbeb
alokkkxpixel <alokpithale4@gmail.com>
2025-10-03 14:10:42 +0530

    CORS Change

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,12 +6,18 @@ const foodpartnerRoutes = require("./routes/food-partner.route");
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
+const allowedOrigins = [
+  "http://localhost:5173", // or 3000 if React
+  "https://food-view-flame.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "hthttps://food-view-flame.vercel.app/",
+    origin: allowedOrigins,
     credentials: true,
   })
 );
+
 const dotenv = require("dotenv");
 dotenv.config();
 app.use(express.json());
